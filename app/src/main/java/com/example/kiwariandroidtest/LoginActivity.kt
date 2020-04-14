76c14b9df1c3ae3ccf36dc1af7b5c54b592c538b
Wildan Kurniadi <danprogramming10@gmail.com>
2020-04-13 17:45:02 -0700

    #01 after adding some firebase dependency, Login Success

--- app/src/main/java/com/example/kiwariandroidtest/LoginActivity.kt
+++ app/src/main/java/com/example/kiwariandroidtest/LoginActivity.kt
@@ -1,12 +1,38 @@
 package com.example.kiwariandroidtest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.kiwariandroidtest.databinding.ActivityLoginBinding
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
-
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+
+        binding.btnLogin.setOnClickListener {
+            auth = FirebaseAuth.getInstance()
+            val email = binding.tietEmail.text.toString()
+            val password = binding.etPassword.text.toString()
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, OnCompleteListener{task  ->
+                if(task.isSuccessful){
+                    Toast.makeText(this, "Successfully Logged In", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+                    finish()
+                }else{
+                    Log.e("Login", task.result.toString())
+                    Toast.makeText(this, "Login Failed "+task.result, Toast.LENGTH_SHORT).show()
+                }
+
+            })
+        }
+
     }
 }
--- app/src/main/java/com/example/kiwariandroidtest/SplashActivity.kt
+++ app/src/main/java/com/example/kiwariandroidtest/SplashActivity.kt
@@ -4,19 +4,23 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.kiwariandroidtest.util.SharedPref
+import com.google.firebase.auth.FirebaseAuth
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         //check the sharedpref whether user already logged in
-        val sharedPref = SharedPref(this)
+//        val sharedPref = SharedPref(this)
+        auth = FirebaseAuth.getInstance()
 
-        if (sharedPref.isLogin()) {
-            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
-        } else {
+        if(auth.currentUser == null){
             startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+            finish()
+        }else{
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
         }
     }
 }
